56b690c3a0ca83975afa43edebeabb070d3c6197
school-frahma7 <frahma7@illinois.edu>
2019-05-09 00:34:23 -0500

    Updates

---
+++ precache-manifest.8125f7e4c1d349ed6448aa6e2bb458d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0520e1e2a449cdfcc50f",
+    "url": "/frahma7/WeatherOutfit/static/js/runtime~main.66c82633.js"
+  },
+  {
+    "revision": "785af206c68801ebe751",
+    "url": "/frahma7/WeatherOutfit/static/js/main.c1d54979.chunk.js"
+  },
+  {
+    "revision": "e41a3cfef437a26b981e",
+    "url": "/frahma7/WeatherOutfit/static/js/2.2ea16842.chunk.js"
+  },
+  {
+    "revision": "785af206c68801ebe751",
+    "url": "/frahma7/WeatherOutfit/static/css/main.6a4e8821.chunk.css"
+  },
+  {
+    "revision": "5ca4156ce51d05de609966f12506a8e7",
+    "url": "/frahma7/WeatherOutfit/index.html"
+  }
+];
